66ef86334ffb7c7bd51e67459f4cc3d2fbe3174c
hsshih <73636009+hsshih@users.noreply.github.com>
2021-02-01 10:38:13 -1000

    Add files via upload

---
+++ Wide_and_Deep_Models/wd_models_jenny/contextual_dataset_wu.py
@@ -0,0 +1,324 @@
+## 01/19/21 Copied from space-bandits, modeified to handle user IDs
+
+"""Define a data buffer for contextual bandit algorithms."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+import numpy as np
+import torch
+import pandas as pd
+from scipy.sparse import coo_matrix
+
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+class ContextualDataset(object):
+    """The buffer is able to append new data, and sample random minibatches."""
+
+    def __init__(self, context_dim, num_actions, memory_size=-1, intercept=False):
+        """Creates a ContextualDataset object.
+        The data is stored in attributes: contexts and rewards.
+        The sequence of taken actions are stored in attribute actions.
+        Args:
+          context_dim: Dimension of the contexts.
+          num_actions: Number of arms for the multi-armed bandit.
+          memory_size: Specify the number of examples to store in memory.
+            if memory_size = -1, all data will be stored.
+          intercept: If True, it adds a constant (1.0) dimension to each context X,
+            at the end.
+        """
+
+        self._context_dim = context_dim
+        self._num_actions = num_actions
+        self.contexts = None
+        self.scaled_contexts = None
+        self.rewards = None
+        self.user_ids = []
+        self.actions = []
+        self.memory_size = memory_size
+        self.intercept = intercept
+        self.scaling_data = []
+
+    def add(self, user_id, context, action, reward):
+        """Adds a new triplet (context, action, reward) to the dataset.
+        The reward for the actions that weren't played is assumed to be zero.
+        Args:
+          user_id: User ID, usually an integer
+          context: A d-dimensional vector with the context.
+          action: Integer between 0 and k-1 representing the chosen arm.
+          reward: Real number representing the reward for the (context, action).
+        """
+        if not isinstance(context, torch.Tensor):
+            context = torch.tensor(context.astype(float))
+        if len(context.shape) > 1:
+            context = context.reshape(-1)
+        if self.intercept:
+            c = context[:]
+            c = torch.cat((c, torch.tensor([1.0]).double()))
+            c = c.reshape((1, self.context_dim + 1))
+        else:
+            if type(context) == type(torch.tensor(0)):
+                c = context[:].reshape((1, self.context_dim))
+            else:
+                c = torch.tensor(context[:]).reshape((1, self.context_dim))
+
+
+
+        if self.contexts is None:
+
+            self.contexts = c
+        else:
+            self.contexts = torch.cat((self.contexts, c))
+
+
+
+        r = torch.zeros((1, self.num_actions))
+        r[0, action] = float(reward)
+        if self.rewards is None:
+            self.rewards = r
+        else:
+            self.rewards = torch.cat((self.rewards, r))
+
+
+        self.actions.append(action)
+        self.user_ids.append(user_id)
+
+        #Drop oldest example if memory constraint
+        if self.memory_size != -1:
+            if self.contexts.shape[0] > self.memory_size:
+                self.contexts = self.contexts[1:, :]
+                self.rewards = self.rewards[1:, :]
+                self.actions = self.actions[1:]
+                self.user_ids = self.user_ids[1:]
+            #Assert lengths match
+            assert len(self.actions) == len(self.rewards)
+            assert len(self.actions) == len(self.contexts)
+            assert len(self.actions) == len(self.user_ids)
+
+    def _replace_data(self, user_ids=None, contexts=None, actions=None, rewards=None):
+        if contexts is not None:
+            self.contexts = contexts
+        if actions is not None:
+            self.actions = actions
+        if rewards is not None:
+            self.rewards = rewards
+        if user_ids is not None:
+            self.user_ids = user_ids
+
+    def _ingest_data(self, user_ids, contexts, actions, rewards):
+        """Ingests bulk data."""
+        if isinstance(rewards, pd.DataFrame) or isinstance(rewards, pd.Series):
+            rewards = rewards.values
+        if isinstance(actions, pd.DataFrame) or isinstance(actions, pd.Series):
+            actions = actions.values
+        if isinstance(user_ids, pd.DataFrame) or isinstance(user_ids, pd.Series):
+            user_ids = user_ids.values
+        if isinstance(contexts, pd.DataFrame) or isinstance(contexts, pd.Series):
+            contexts = contexts.values
+
+        if not isinstance(rewards, torch.Tensor):
+            rewards = torch.tensor(rewards)
+        if not isinstance(actions, torch.Tensor):
+            actions = torch.tensor(actions)
+        if not isinstance(user_ids, torch.Tensor):
+            user_ids = torch.tensor(user_ids)
+        if not isinstance(contexts, torch.Tensor):
+            contexts = torch.tensor(contexts)
+
+        data_length = len(rewards)
+
+        if self.memory_size != -1:
+            if data_length + len(self.rewards) > self.memory_size:
+                print('Cannot add more examples: ')
+                raise Exception('Too many examples for specified memory_size.')
+
+        try:
+            contexts = contexts.reshape(-1, self.context_dim)
+        except:
+            print('Got bad data contexts shape: ', contexts.shape)
+            raise Exception('Expected: ({}, {})'.format(data_length, self.context_dim))
+
+        if self.intercept:
+            #add intercepts
+            contexts = torch.cat([contexts, torch.ones((data_length, 1)).double()], dim=1)
+
+        try:
+            assert len(contexts) == data_length
+            assert len(actions) == data_length
+        except AssertionError:
+            raise AssertionError('Data lengths inconsistent.')
+
+        if self.contexts is None:
+            self.contexts = contexts
+        else:
+            self.contexts = torch.cat((self.contexts, contexts))
+
+        rewards_array = coo_matrix((np.array(rewards), (np.arange(data_length), np.array(actions)))).toarray()
+        rewards_array = torch.tensor(rewards_array).float()
+        if self.rewards is None:
+            self.rewards = rewards_array
+        else:
+            self.rewards = torch.cat((self.rewards, rewards_array.float()))
+
+        self.actions = self.actions + list(actions)
+        self.user_ids = self.user_ids + list(user_ids)
+
+    def get_batch(self, batch_size=512):
+        """Returns a random minibatch of (contexts, rewards) with batch_size."""
+        n, _ = self.contexts.shape
+        ind = np.random.choice(range(n), batch_size)
+        return torch.tensor(self.user_ids)[ind], self.contexts[ind, :], self.rewards[ind, :], torch.tensor(self.actions)[ind]
+
+    def get_data(self, action):
+        """Returns all (user_id, context, reward) where the action was played."""
+        n, _ = self.contexts.shape
+        ind = np.argwhere(np.array(self.actions) == action).reshape(-1)
+        return torch.tensor(self.user_ids)[ind], self.contexts[ind, :], self.rewards[ind, action]
+    
+    def get_user_freq(self, user_id):
+        """Returns the number of times an user_id occurs in data"""
+        user_id = torch.tensor(user_id).tolist()
+        user_arr = np.array(self.user_ids)
+        user_freq = len(np.where(user_arr == user_id)[0])
+        return user_freq
+
+    def get_data_with_weights(self):
+        """Returns all observations with one-hot weights for actions."""
+        weights = torch.zeros((self.contexts.shape[0], self.num_actions))
+        a_ind = np.array([(i, val) for i, val in enumerate(self.actions)])
+        weights[a_ind[:, 0], a_ind[:, 1]] = 1.0
+        return torch.tensor(self.user_ids)[ind], self.contexts, self.rewards, weights
+
+    def get_batch_with_weights(self, batch_size, scaled=False):
+        """Returns a random mini-batch with one-hot weights for actions."""
+
+        n, _ = self.contexts.shape
+        if self.memory_size == -1:
+            # use all the data
+            ind = np.random.choice(range(n), batch_size)
+        else:
+            # use only buffer (last buffer_s obs)
+            ind = np.random.choice(range(max(0, n - self.memory_size), n), batch_size)
+
+        weights = torch.zeros((batch_size, self.num_actions))
+        sampled_actions = torch.tensor(self.actions)[ind]
+        a_ind = torch.tensor([(i, val) for i, val in enumerate(sampled_actions)])
+        weights[a_ind[:, 0], a_ind[:, 1]] = 1.0
+        if scaled:
+            ctx = self.scaled_contexts[ind, :]
+        else:
+            ctx = self.contexts[ind, :]
+        return torch.tensor(self.user_ids)[ind], ctx, self.rewards[ind, :], weights
+
+
+    def get_batch_with_weights_recent(self, batch_size, n_recent=0, scaled=False):
+        """Returns a random mini-batch with one-hot weights for actions."""
+        
+        n, _ = self.contexts.shape
+        if self.memory_size == -1:
+            # use all the data
+            ind = np.random.choice(range(n), batch_size)
+        else:
+            # use only buffer (last buffer_s obs)
+            ind = np.random.choice(range(max(0, n - self.memory_size), n), batch_size)
+        
+        if n_recent > 0:
+            ## returns the n_recent most recently added data points
+            if n_recent > batch_size:
+                n_recent = batch_size
+            if n_recent > n:
+                n_recent = n
+            ind[:n_recent] = range(n-n_recent,n)
+        
+        weights = torch.zeros((batch_size, self.num_actions))
+        sampled_actions = torch.tensor(self.actions)[ind]
+        a_ind = torch.tensor([(i, val) for i, val in enumerate(sampled_actions)])
+        weights[a_ind[:, 0], a_ind[:, 1]] = 1.0
+        if scaled:
+            ctx = self.scaled_contexts[ind, :]
+        else:
+            ctx = self.contexts[ind, :]
+        return torch.tensor(self.user_ids)[ind], ctx, self.rewards[ind, :], weights
+
+    def num_points(self, f=None):
+        """Returns number of points in the buffer (after applying function f)."""
+        if f is not None:
+            return f(self.contexts.shape[0])
+        return self.contexts.shape[0]
+
+    def scale_contexts(self, contexts=None):
+        """
+        Performs mean/std scaling operation on contexts.
+        if contexts is provided as argument, returns scaled version
+            (scaled by statistics of data in dataset.)
+        """
+        means = self.contexts.mean(dim=0)
+        stds = self.contexts.std(dim=0)
+        stds[stds==0] = 1
+        self.scaled_contexts = self.contexts.clone()
+        for col in range(self._context_dim):
+            self.scaled_contexts[:, col] -= means[col]
+            self.scaled_contexts[:, col] /= stds[col]
+        if contexts is not None:
+            if not isinstance(contexts, torch.Tensor):
+                contexts = torch.tensor(contexts)
+            result = contexts
+            for col in range(self._context_dim):
+                result[:, col] -= means[col]
+                result[:, col] /= stds[col]
+            return result
+
+    def get_contexts(self, scaled=False):
+        if scaled:
+            return self.scaled_contexts
+        else:
+            return self.contexts
+
+    def get_user_ids(self):
+        return torch.tensor(self.user_ids)
+
+    def __len__(self):
+        return len(self.actions)
+
+    @property
+    def context_dim(self):
+        return self._context_dim
+
+    @property
+    def num_actions(self):
+        return self._num_actions
+
+    @property
+    def contexts(self):
+        return self._contexts
+
+    @contexts.setter
+    def contexts(self, value):
+        self._contexts = value
+
+    @property
+    def actions(self):
+        return self._actions
+    
+    @actions.setter
+    def actions(self, value):
+        self._actions = value
+
+    @property
+    def rewards(self):
+        return self._rewards
+
+    @rewards.setter
+    def rewards(self, value):
+        self._rewards = value
+
+    @property
+    def user_ids(self):
+        return self._user_ids
+    
+    @user_ids.setter
+    def user_ids(self, value):
+        self._user_ids = value
---
+++ Wide_and_Deep_Models/wd_models_jenny/toy_problem_wu.py
@@ -0,0 +1,257 @@
+## 01/19/21 Copied from space-bandits, modified to return user_id
+
+import numpy as np
+import pandas as pd
+from random import random, randint
+
+def get_customer(ctype=None):
+    """Customers come from two feature distributions.
+    Class 1: mean age 25, var 5 years, min age 18
+             mean ARPU 100, var 15
+    Class 2: mean age 45, var 6 years
+             mean ARPU 50, var 25
+    """
+    if ctype is None:
+        if random() > .5: #coin toss
+            ctype = 1
+        else:
+            ctype = 2
+    age = 0
+    ft = -1
+    if ctype == 1:
+        while age < 18:
+            age = np.random.normal(25, 5)
+        while ft < 0:
+            ft = np.random.normal(100, 15)
+        user_id = np.random.randint(0,20)
+    if ctype == 2:
+        while age < 18:
+            age = np.random.normal(45, 6)
+        while ft < 0:
+            ft = np.random.normal(50, 25)
+        user_id = np.random.randint(20,40)
+    age = round(age)
+    return ctype, (age, ft), user_id
+
+def get_rewards(customer):
+    """
+    There are three actions:
+    promo 1: low value. 10 dollar if accept
+    promo 2: mid value. 25 dollar if accept
+    promo 3: high value. 100 dollar if accept
+
+    Both groups are unlikely to accept promo 2.
+    Group 1 is more likely to accept promo 1.
+    Group 2 is slightly more likely to accept promo 3.
+
+    The optimal choice for group 1 is promo 1; 90% acceptance for
+    an expected reward of 9 dollars each.
+    Group 2 accepts with 25% rate for expected 2.5 dollar reward
+
+    The optimal choice for group 2 is promo 3; 20% acceptance for an expected
+    reward of 20 dollars each.
+    Group 1 accepts with 2% for expected reward of 2 dollars.
+
+    The least optimal choice in all cases is promo 2; 10% acceptance rate for both groups
+    for an expected reward of 2.5 dollars.
+    """
+    if customer[0] == 1: #group 1 customer
+        if random() > .1:
+            reward1 = 10
+        else:
+            reward1 = 0
+        if random() > .90:
+            reward2 = 25
+        else:
+            reward2 = 0
+        if random() > .98:
+            reward3 = 100
+        else:
+            reward3 = 0
+    if customer[0] == 2: #group 2 customer
+        if random() > .75:
+            reward1 = 10
+        else:
+            reward1 = 0
+        if random() > .90:
+            reward2 = 25
+        else:
+            reward2 = 0
+        if random() > .80:
+            reward3 = 100
+        else:
+            reward3 = 0
+    return np.array([reward1, reward2, reward3])
+
+def get_cust_reward():
+    """returns a customer and reward vector"""
+    cust = get_customer()
+    reward = get_rewards(cust)
+    fts = cust[1]
+    return np.array([fts])/100, reward
+
+def generate_dataframe(n_rows):
+    df = pd.DataFrame()
+    ages = []
+    ARPUs = []
+    actions = []
+    rewards = []
+    user_ids = []
+    for i in range(n_rows):
+        cust = get_customer()
+        reward_vec = get_rewards(cust)
+        context = np.array([cust[1]])
+        user_id = cust[2]
+        
+        ages.append(context[0, 0])
+        ARPUs.append(context[0, 1])
+        action = np.random.randint(0,3)
+        actions.append(action)
+        reward = reward_vec[action]
+        rewards.append(reward)
+        user_ids.append(user_id)
+
+    df['age'] = ages
+    df['ARPU'] = ARPUs
+    df['action'] = actions
+    df['reward'] = rewards
+    df['user_id'] = user_ids
+
+    return df
+
+def get_customer_nl(ctype=None):
+    """Customers come from two feature distributions.
+    Class 1: mean age 25, var 5 years, min age 18
+             mean ARPU 100, var 15
+    Class 2: mean age 45, var 6 years
+             mean ARPU 50, var 25
+    """
+    if ctype is None:
+        ctype = randint(0,2)
+    age = 0
+    ft = -1
+    if ctype == 0:
+        while age < 18:
+            age = np.random.normal(25, 5)
+            ft = 125 - .1*(age-25)*(age-25) + np.random.normal(0, 4)
+        user_id = np.random.randint(0,20)
+    if ctype == 1:
+        while age < 18:
+            age = np.random.normal(35, 2)
+        while ft < 0:
+            ft = np.random.normal(75, 3)
+        user_id = np.random.randint(20,40)
+    if ctype == 2:
+        while age < 18:
+            age = np.random.normal(45, 6)
+            ft = 25 + .25*(age-45)*(age-45) + np.random.normal(0, 4)
+        user_id = np.random.randint(40,60)
+    age = round(age)
+    return ctype, (age, ft), user_id
+
+def get_rewards_nl(customer):
+    """
+    There are three actions:
+    promo 1: low value. 10 dollar if accept
+    promo 2: mid value. 25 dollar if accept
+    promo 3: high value. 100 dollar if accept
+
+    Expected Value Matrix:
+
+           group1|group2|group3
+    ----------------------------
+    promo1|  $9  |  $1  |  $1
+    ----------------------------
+    promo2| $2.5 | $12.5| $1.25
+    ----------------------------
+    promo3| $1   |  $5  | $25
+
+    We can see each group has an optimal choice.
+    """
+    if customer[0] == 0: #group 1 customer
+        if random() > .1:
+            reward1 = 10
+        else:
+            reward1 = 0
+        if random() > .90:
+            reward2 = 25
+        else:
+            reward2 = 0
+        if random() > .99:
+            reward3 = 100
+        else:
+            reward3 = 0
+    if customer[0] == 1: #group 2 customer
+        if random() > .9:
+            reward1 = 10
+        else:
+            reward1 = 0
+        if random() > .50:
+            reward2 = 25
+        else:
+            reward2 = 0
+        if random() > .95:
+            reward3 = 100
+        else:
+            reward3 = 0
+    if customer[0] == 2: #group 3 customer
+        if random() > .9:
+            reward1 = 10
+        else:
+            reward1 = 0
+        if random() > .95:
+            reward2 = 25
+        else:
+            reward2 = 0
+        if random() > .75:
+            reward3 = 100
+        else:
+            reward3 = 0
+    return np.array([reward1, reward2, reward3])
+
+def get_cust_reward_nl():
+    """
+    returns a customer and reward vector
+    for nonlinear problem
+    """
+    cust = get_customer()
+    reward = get_rewards(cust)
+    age = cust[1]
+    return np.array([age]), reward
+
+def generate_biased_dataframe(n_rows):
+    df = pd.DataFrame()
+    ages = []
+    ARPUs = []
+    actions = []
+    rewards = []
+    user_ids = []
+    
+    for i in range(n_rows):
+        cust = get_customer()
+        reward_vec = get_rewards(cust)
+        context = np.array([cust[1]])
+        age = context[0, 0]
+        ARPU = context[0, 1]
+        user_id = cust[2]
+        
+        ages.append(age)
+        ARPUs.append(ARPU)
+        if ARPU <= 50:
+            action = 0
+        elif ARPU <= 100:
+            action = 1
+        else:
+            action = 2
+        actions.append(action)
+        reward = reward_vec[action]
+        rewards.append(reward)
+        user_ids.append(user_id)
+
+    df['age'] = ages
+    df['ARPU'] = ARPUs
+    df['action'] = actions
+    df['reward'] = rewards
+    df['user_id'] = user_ids
+
+    return df
